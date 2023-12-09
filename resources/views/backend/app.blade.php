87dba8a79e1dd2ce0f7e3c9c3925c8bd695f968b
sawal123 <sawalinto@gmail.com>
2023-12-09 12:06:09 +0700

    app blade

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -7,10 +7,10 @@
     <meta charset="UTF-8">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Sash – Bootstrap 5  Admin & Dashboard Template">
-    <meta name="author" content="Spruko Technologies Private Limited">
+    <meta name="description" content="Login Gotik">
+    <meta name="author" content="Gotik">
     <meta name="keywords"
-        content="admin,admin dashboard,admin panel,admin template,bootstrap,clean,dashboard,flat,jquery,modern,responsive,premium admin templates,responsive admin,ui,ui kit.">
+        content="Login Gotik">
 
     <!-- FAVICON -->
     <link rel="shortcut icon" type="image/x-icon" href="{{asset('storage/logo/'. $logo[0]->icon)}}">
