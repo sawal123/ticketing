e7af7a5b32babb1da665e032181c7fd533286f6d
sawal123 <sawalinto@gmail.com>
2026-03-19 10:41:36 +0700

    Feat: update home

--- resources/views/frontend/page/home/menu.blade.php
+++ resources/views/frontend/page/home/menu.blade.php
@@ -1,14 +1,14 @@
 <div class="container mx-auto">
     <div class="row">
         @foreach ($events as $event)
-            <div class="col-3 d-flex justify-content-center mb-4">
+            <div class="col-12 col-md-6 col-lg-3 d-flex justify-content-center mb-4">
 
                 <div class="card bg-dark border mx-2 my-2 d-inline-block" style="width: 18rem; border-radius: 4%;">
                     <div class="fugu--card-thumb">
                         @if ($event->status === 'close')
                             <div class="border"
                                 style="position: absolute; top: 0; left: 0; z-index: 99; border-radius: 10px 0 10px 0; background-color: rgb(128, 0, 0); padding: 5px;">
-                                <p class="fw-bold" style="color: white">Close</p>
+                                <p class="fw-bold mb-0" style="color: white">Close</p>
                             </div>
                         @endif
                         <img src="{{ asset('/storage/cover/' . $event->cover) }}" class="card-img-top" loading="lazy"
@@ -16,8 +16,8 @@
                             alt="...">
                     </div>
                     <div class="card-body fugu--card-data text-start">
-                        <h6 style="color: white"><a href="{{ url('/ticket/' . $event->slug) }}"
-                                class="text-white">{{ $event->event }}</a>
+                        <h6 style="color: white">
+                            <a href="{{ url('/ticket/' . $event->slug) }}" class="text-white">{{ $event->event }}</a>
                         </h6>
                         <a style="color: white;" class="mb-2" href="{{ $event->map }}">
                             <p>{{ implode(' ', array_slice(explode(' ', $event->alamat), 0, 3)) . '...' }}</p>
@@ -43,17 +43,15 @@
                             <a class="fugu--btn btn-sm bg-white" href="{{ url('/ticket/' . $event->slug) }}">Beli</a>
                         </div>
                         <hr class="my-2">
-                        <p>By: {{ $event->name }}</p>
+                        <p class="mb-0">By: {{ $event->name }}</p>
                     </div>
                 </div>
-
-
 
             </div>
         @endforeach
     </div>
 
-    <div class="fugu--portfolio-btn">
+    <div class="fugu--portfolio-btn text-center mt-4">
         <a class="fugu--outline-btn" href="{{ url('/search') }}"><span>View All Events</span></a>
     </div>
 </div>
