c9c2a71daeea155c35c8a1f438d1c1578681335b
sawal123 <sawalinto@gmail.com>
2026-04-02 17:32:49 +0700

    feat: add cardEventSearch component with new event button

--- resources/views/penyewa/molecul/cardEventSearch.blade.php
+++ resources/views/penyewa/molecul/cardEventSearch.blade.php
@@ -2,10 +2,8 @@
     <div class="card-body p-4">
         <div class="row">
             <div class="col-xl-5 col-lg-8 col-md-8 col-sm-8">
-
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-
             </div>
             <div class="col-xl-3 col-lg-12">
                 <a href="{{url('/dashboard/event/addEvent')}}" class="btn btn-primary btn-block float-end my-2"><i
